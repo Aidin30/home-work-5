539da65badfeebd1d9e759c042875a349581d87e
Aidin30 <azamatuuluaidin@gmail.com>
2024-02-16 19:54:53 +0600

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,81 @@
+// int square(int number){
+//   return number*number;
+// }
+// void main(){
+//   square(4);
+//   int result = square(4);
+//   result =square(result);
+//   print(result);
+// }
+
+// int summ(){
+//   return 10+4;
+// }
+// void main(){
+//   print(summ());
+// }
+
+
+// double divide (double firstNum, double secondNum, double thirdNum){
+// double result= (firstNum - secondNum)/ thirdNum;
+// return result;
+// }
+// void main(){
+//   double firstNum = 2;
+//   double secondNum =3;
+//   double thirdNum =4;
+// double result = divide(firstNum, secondNum, thirdNum);
+// }
+// void printResult(double result) {
+//   print( result);
+// }
+
+
+
+// int summ(int min){
+//   return min * 60;
+
+// }
+// void main(){
+//   print(summ(60));
+// }
+
+
+// int summ(List array){
+   
+//    return array.first;
+   
+// }
+// void main(){
+//   List array = [1, 3, 4, 5, 6];
+//   print(summ(array));
+
+// }
+
+
+// void main() {
+//   bool myBro = true;
+
+
+//   if (myBro) {
+//     print("Переменная имеет значение ");
+//   } 
+// }
+
+
+bool myNum(int number) {
+  return number <= 0;
+}
+
+void main() {
+  
+  print(myNum(1)); 
+  print(myNum(0));  
+  print(myNum(-2)); 
+}
+
+  
+  
+
+
+
