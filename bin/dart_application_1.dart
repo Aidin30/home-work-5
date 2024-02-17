c5e147e3b2068aec365d05874f3249ea7ea7451e
Aidin30 <azamatuuluaidin@gmail.com>
2024-02-18 01:25:06 +0600

    first commit

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,81 +1,76 @@
-// int square(int number){
-//   return number*number;
-// }
-// void main(){
-//   square(4);
-//   int result = square(4);
-//   result =square(result);
-//   print(result);
-// }
-
-// int summ(){
-//   return 10+4;
-// }
-// void main(){
-//   print(summ());
-// }
-
-
-// double divide (double firstNum, double secondNum, double thirdNum){
-// double result= (firstNum - secondNum)/ thirdNum;
-// return result;
-// }
-// void main(){
-//   double firstNum = 2;
-//   double secondNum =3;
-//   double thirdNum =4;
-// double result = divide(firstNum, secondNum, thirdNum);
-// }
-// void printResult(double result) {
-//   print( result);
-// }
-
-
-
-// int summ(int min){
-//   return min * 60;
-
-// }
-// void main(){
-//   print(summ(60));
-// }
-
-
-// int summ(List array){
-   
-//    return array.first;
-   
-// }
-// void main(){
-//   List array = [1, 3, 4, 5, 6];
-//   print(summ(array));
-
-// }
-
+// import 'dart:io';
 
 // void main() {
-//   bool myBro = true;
-
-
-//   if (myBro) {
-//     print("Переменная имеет значение ");
-//   } 
+//   print('register your own login');
+//   String logiN = stdin.readLineSync() ?? '';
+//   print('enter your password');
+//   String passWord = stdin.readLineSync() ?? '';
+//   enter(logiN, passWord);
 // }
 
+// void enter(String login, String password) {
+//   String enteredPassword = '';
+//   String enteredLogin = '';
+//   do {
+//     print('enter your login');
+//     enteredLogin = stdin.readLineSync() ?? '';
 
-bool myNum(int number) {
-  return number <= 0;
+//   } while (enteredLogin != login);
+//    do {
+//       print('enter ur password');
+//       enteredPassword = stdin.readLineSync() ?? '';
+//     } while (enteredPassword != password);
+// }
+
+// import 'dart:io';
+
+// void main() {
+//   int randomNumber = 0;
+//   List number = [];
+//   while (randomNumber != 0) {
+//     int summ = 0;
+//     int count =0;
+//     randomNumber = int.tryParse(stdin.readLineSync() ?? '') ?? 0;
+//     number.add(randomNumber);
+//   }
+// }
+// import 'dart:io';
+
+// void main() {
+//   print('enter your first number');
+//   int opening = int.parse(stdin.readLineSync()!);
+
+//   print('enter your second number');
+//   int ending = int.parse(stdin.readLineSync()!);
+
+//   int sum = 0;
+//   int number=opening;
+
+//   do {
+//     if (number % 2 != 0) {
+//       sum += number;
+//     }
+//     number++;
+//   } while (number <= ending);
+
+//   print('$opening $ending $sum summ of odd numbers ');
+// }
+import 'dart:io';
+
+void main(){
+String enterNumber ='';
+int summ =0;
+List<int> number =[];
+while(enterNumber!='0'){  
+  enterNumber =stdin.readLineSync()??'';
+  number.add(int.tryParse(enterNumber)??0);
+  summ+= int.tryParse(enterNumber)??0;
+   }
+  print('$summ summ ${number.length} ${summ/(number.length)}');
 }
 
-void main() {
-  
-  print(myNum(1)); 
-  print(myNum(0));  
-  print(myNum(-2)); 
-}
 
-  
-  
+
 
 
 
