34cc9a5247d1d2789a427b9046c03096d84b3c63
Aidin30 <azamatuuluaidin@gmail.com>
2024-02-18 12:54:09 +0600

    first commit

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,76 +1,100 @@
-// import 'dart:io';
+// void main(){
+// List<int> a  = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+// List<int> b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// List<int> result =[];
+// for(int i=0; i<b.length; i++){
+//  if(a.contains(b[i])){
+//   result.add(b[i]);
+//  }
+// }
+// result=result.toSet().toList();
+// print(result);
 
-// void main() {
-//   print('register your own login');
-//   String logiN = stdin.readLineSync() ?? '';
-//   print('enter your password');
-//   String passWord = stdin.readLineSync() ?? '';
-//   enter(logiN, passWord);
 // }
 
-// void enter(String login, String password) {
-//   String enteredPassword = '';
-//   String enteredLogin = '';
-//   do {
-//     print('enter your login');
-//     enteredLogin = stdin.readLineSync() ?? '';
-
-//   } while (enteredLogin != login);
-//    do {
-//       print('enter ur password');
-//       enteredPassword = stdin.readLineSync() ?? '';
-//     } while (enteredPassword != password);
+// void main(){
+// List<int> a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+// List<int> result=[];
+// for(int i=0; i<a.length; i++){
+//   if(a[i].isEven){
+//     result.add(a[i]);
+//   }
+// }
+// print(result);
 // }
 
-// import 'dart:io';
-
-// void main() {
-//   int randomNumber = 0;
-//   List number = [];
-//   while (randomNumber != 0) {
-//     int summ = 0;
-//     int count =0;
-//     randomNumber = int.tryParse(stdin.readLineSync() ?? '') ?? 0;
-//     number.add(randomNumber);
+// void main(){
+//   print(func('a', 'dart'));
+// }
+// int func(String letter, String word){
+//   int counter=0;
+//   for(int i=0; i<word.length; i++){
+//     if(word[i].toLowerCase()==letter.toLowerCase()){
+//       counter++;
+//     }
+//   }
+//   return counter;
+// }
+// void main(){
+//   for(int i =10; i<=20; i++){
+//     print(i*i);
 //   }
 // }
 // import 'dart:io';
 
-// void main() {
-//   print('enter your first number');
-//   int opening = int.parse(stdin.readLineSync()!);
-
-//   print('enter your second number');
-//   int ending = int.parse(stdin.readLineSync()!);
-
-//   int sum = 0;
-//   int number=opening;
-
-//   do {
-//     if (number % 2 != 0) {
-//       sum += number;
-//     }
-//     number++;
-//   } while (number <= ending);
-
-//   print('$opening $ending $sum summ of odd numbers ');
+// void main(){
+// int n = int.tryParse(stdin.readLineSync()??'')??0;
+// int result =0;
+// for(int i=1; i<=n; i++){
+//   result+=i;
 // }
-import 'dart:io';
+// print(result);
+// }
+
+// import 'dart:io';
+
+// void main() {
+//   stdout.write('vvedite summu vklada ');
+//   double s = double.parse(stdin.readLineSync()!);
+
+//   stdout.write('Введите количество лет: ');
+//   int n = int.parse(stdin.readLineSync()!);
+
+//   double sum = totalAmount(s, n);
+
+//   print('summa vklada cherez  $n let: $sum rubli');
+// }
+
+// double totalAmount(double principal, int years) {
+//   double rate = 0.03;
+//   return principal * (1 + rate * years);
+// }
+
+// void main() {
+//   for (int i = 20; i <= 50; i++) {
+//     if (i % 3 == 0 && i % 5 != 0) {
+//       print(i);
+//     }
+//   }
+// }
+
+// void main(){
+//   for (int i=1; i<=50; i++){
+//     if(i%5 ==0 && i%7==0 ){
+//       print(i);
+//     }
+//   }
+// }
 
 void main(){
-String enterNumber ='';
-int summ =0;
-List<int> number =[];
-while(enterNumber!='0'){  
-  enterNumber =stdin.readLineSync()??'';
-  number.add(int.tryParse(enterNumber)??0);
-  summ+= int.tryParse(enterNumber)??0;
-   }
-  print('$summ summ ${number.length} ${summ/(number.length)}');
+  print('te chisla kotorye delyatsya na 4 , no ne delyatsya na 6');
+  for(int i=10; i<=99; i++){
+    if(i%4==0 && i%6!=0){
+      print(i);
+    }
+  }
+
+
+
 }
-
-
-
-
-
 
